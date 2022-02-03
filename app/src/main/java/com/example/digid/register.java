530034d36ce9c23cc1b4dafab22889cc20247836
drj63 <drj63@scarletmail.rutgers.edu>
2022-02-03 12:24:07 -0500

    Basic register, home, and main page

--- app/src/main/java/com/example/digid/register.java
+++ app/src/main/java/com/example/digid/register.java
@@ -14,59 +14,59 @@ public class register extends AppCompatActivity {
     EditText mEmail, mPassword;
     Button mLoginBtn;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.register);
-
-        }
-
-        public void onButtonClick(View v)
-        {
-            mEmail = findViewById(R.id.inputEmail);
-            mPassword = findViewById(R.id.editTextPassword);
-            mLoginBtn = findViewById(R.id.bRegisterAttempt);
-
-            switch (v.getId())
-            {
-                case R.id.bRegisterAttempt:
-                {
-                    String email = mEmail.getText().toString().trim();
-                    String password = mPassword.getText().toString().trim();
-
-                    if(TextUtils.isEmpty(email))
-                    {
-                        mEmail.setError("Email is Required");
-                        return;
-                    }
-                    //add @ check
-                    //add scarletmail check
-
-                    if(TextUtils.isEmpty(password))
-                    {
-                        mPassword.setError("Password is Required");
-                        return;
-                    }
-                    if(password.length() < 6)
-                    {
-                        mPassword.setError("Password must be greater than 5 characters!");
-                        return;
-                    }
-                    else
-                    {
-                        Toast.makeText(register.this, "User Created", Toast.LENGTH_SHORT).show();
-                        Bundle bundle=new Bundle();
-                        Intent intent = new Intent(getApplicationContext(),home.class);
-                        intent.putExtras(bundle);
-                        startActivity(intent);
-
-                    };
-
-                }
-            }
-
-
-        }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
 
     }
+
+    public void onButtonClick(View v)
+    {
+        mEmail = findViewById(R.id.inputEmail);
+        mPassword = findViewById(R.id.editTextPassword);
+        mLoginBtn = findViewById(R.id.bRegisterAttempt);
+
+        switch (v.getId())
+        {
+            case R.id.bRegisterAttempt:
+            {
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email))
+                {
+                    mEmail.setError("Email is Required");
+                    return;
+                }
+                //add @ check
+                //add scarletmail check
+
+                if(TextUtils.isEmpty(password))
+                {
+                    mPassword.setError("Password is Required");
+                    return;
+                }
+                if(password.length() < 6)
+                {
+                    mPassword.setError("Password must be greater than 5 characters!");
+                    return;
+                }
+                else
+                {
+                    Toast.makeText(register.this, "User Created", Toast.LENGTH_SHORT).show();
+                    Bundle bundle=new Bundle();
+                    Intent intent = new Intent(getApplicationContext(),home.class);
+                    intent.putExtras(bundle);
+                    startActivity(intent);
+
+                };
+
+            }
+        }
+
+
+    }
+
+}
 
