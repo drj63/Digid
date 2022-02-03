fa37c7cc325082366917fcc91a7cc0611d20607a
drj63 <drj63@scarletmail.rutgers.edu>
2022-02-03 12:48:37 -0500

    please work

--- app/src/main/java/com/example/digid/home.java
+++ app/src/main/java/com/example/digid/home.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class home extends AppCompatActivity {
-//test
+//yessir
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
