3fc83d727c340dc9ac8f5f2bbda239e5a7ddcaae
drj63 <drj63@scarletmail.rutgers.edu>
2022-02-08 12:30:52 -0500

    verify email page

---
+++ app/src/main/java/com/example/digid/verify_email.java
@@ -0,0 +1,19 @@
+package com.example.digid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class verify_email extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.verifyemail);
+    }
+
+    public void onButtonClick(View view) {
+
+    }
+}
