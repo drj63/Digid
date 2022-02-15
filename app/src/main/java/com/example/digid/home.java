1a5fbd40979f04d4c08931966fc7c497fcfaf203
Aswathy Aji <aswathyaji13@gmail.com>
2022-02-15 12:44:12 -0500

    home and qr code functions for connections

--- app/src/main/java/com/example/digid/home.java
+++ app/src/main/java/com/example/digid/home.java
@@ -4,21 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.WindowManager;
 
 public class home extends AppCompatActivity {
 //yessir YAYAYAYAYAY
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.home);
+
     }
     public void onButtonClick (View v)
     {
         if(v.getId()== R.id.bDigitalID)
         {
-            Intent i=new Intent(home.this, qr_code.class );
-            startActivity(i);
+            new Handler().postDelayed(new Runnable(){
+
+                @Override
+                public void run() {
+                    Intent i = new Intent(home.this,qr_code.class);
+                    startActivity(i);
+                    finish();
+                }
+            }, 2000);
+
         }
         if(v.getId()== R.id.bLocations)
         {
--- app/src/main/java/com/example/digid/qr_code.java
+++ app/src/main/java/com/example/digid/qr_code.java
@@ -2,13 +2,24 @@ package com.example.digid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class qr_code extends AppCompatActivity {
-
+    private Button generateQR;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr_code);
+        generateQR = findViewById(R.id.generate);
+        generateQR.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(qr_code.this,GenerateQR.class);
+                startActivity(i);
+            }
+        });
     }
 }
