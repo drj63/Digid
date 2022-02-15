9e96ffb34ea5d95032cf81d544b9d772c2b52974
Aswathy Aji <aswathyaji13@gmail.com>
2022-02-15 10:52:44 -0500

    qr class and locations class

--- app/src/main/java/com/example/digid/home.java
+++ app/src/main/java/com/example/digid/home.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class home extends AppCompatActivity {
 //yessir YAYAYAYAYAY
@@ -11,5 +12,19 @@ public class home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+    }
+    public void onButtonClick (View v)
+    {
+        if(v.getId()== R.id.bDigitalID)
+        {
+            Intent i=new Intent(home.this, qrCode.class );
+            startActivity(i);
+        }
+        if(v.getId()== R.id.bLocations)
+        {
+            Intent i=new Intent(home.this, locations.class );
+            startActivity(i);
+
+        }
     }
 }
