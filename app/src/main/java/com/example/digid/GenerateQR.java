7e994273fa63c9fe566d939cb56e61e5be71f41a
Aswathy Aji <aswathyaji13@gmail.com>
2022-02-16 22:46:26 -0500

    qr page

--- app/src/main/java/com/example/digid/GenerateQR.java
+++ app/src/main/java/com/example/digid/GenerateQR.java
@@ -3,12 +3,23 @@ package com.example.digid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class GenerateQR extends AppCompatActivity {
+
+    private TextView qrCodeTV;
+    private ImageView qrCodeIV;
+    private TextInputEditText dataEDT;
+    private Button generateQRbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generate_qr);
+
     }
 }
