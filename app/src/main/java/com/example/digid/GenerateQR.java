1a072bdcdb237ecfedfd3c6727c4e36215dfdcde
Aswathy Aji <aswathyaji13@gmail.com>
2022-02-18 11:24:02 -0500

    qr code works with text woooooooooo

--- app/src/main/java/com/example/digid/GenerateQR.java
+++ app/src/main/java/com/example/digid/GenerateQR.java
@@ -2,12 +2,22 @@ package com.example.digid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.zxing.WriterException;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
 
 public class GenerateQR extends AppCompatActivity {
 
@@ -15,11 +25,44 @@ public class GenerateQR extends AppCompatActivity {
     private ImageView qrCodeIV;
     private TextInputEditText dataEDT;
     private Button generateQRbtn;
+    private QRGEncoder qrgEncoder;
+    private Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generate_qr);
+        qrCodeTV = findViewById(R.id.idTVGenerateQR);
+        qrCodeIV = findViewById(R.id.idIVQRCode);
+        dataEDT = findViewById(R.id.idEdtData);
+        generateQRbtn = findViewById(R.id.idGenButton);
+        generateQRbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String data = dataEDT.getText().toString();
+                if(data.isEmpty()){
+                    Toast.makeText(GenerateQR.this, "Enter email in scarletmail format to get digital RUID", Toast.LENGTH_SHORT).show();
+                }else{
+                    WindowManager manager =(WindowManager) getSystemService(WINDOW_SERVICE);
+                    Display display = manager.getDefaultDisplay();
+                    Point point = new Point();
+                    display.getSize(point);
+                    int width = point.x;
+                    int height = point.y;
+                    int dimen = width<height ? width:height;
+                    dimen = dimen * 3/4;
 
+                    qrgEncoder = new QRGEncoder(dataEDT.getText().toString(),null, QRGContents.Type.TEXT,dimen);
+                    try{
+                        bitmap = qrgEncoder.encodeAsBitmap();
+                        qrCodeTV.setVisibility(View.GONE);
+                        qrCodeIV.setImageBitmap(bitmap);
+                    }catch(WriterException e){
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
     }
 }
