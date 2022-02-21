c9d64924be40e029a9692e3efb3d3863828f16bc
drj63 <drj63@scarletmail.rutgers.edu>
2022-02-20 19:26:52 -0500

    general ui changes and easter egg added :) //note: NEED TO IMPROVE SPEED

--- app/src/main/java/com/example/digid/home.java
+++ app/src/main/java/com/example/digid/home.java
@@ -1,17 +1,27 @@
 package com.example.digid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.view.ViewCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
 
 public class home extends AppCompatActivity {
-//yessir YAYAYAYAYAY
+    ImageView img;
+    int count = 0;
+
+    //yessir YAYAYAYAYAY
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.home);
@@ -38,5 +48,17 @@ public class home extends AppCompatActivity {
             startActivity(i);
 
         }
+
+        //dont worry about this code ;)
+        if(v.getId()== R.id.imageView2)
+        {
+                count++;
+
+                if (count == 5) {
+                    img = (ImageView) findViewById(R.id.imageView2);
+                    img.setImageResource(R.drawable.java);
+                }
+        }
+
     }
 }
