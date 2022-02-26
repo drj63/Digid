11f0c96d9ab0f3643576c6ce3150f79a960ebb7f
Aswathy Aji <aswathyaji13@gmail.com>
2022-02-26 10:49:04 -0500

    first part location

--- app/src/main/java/com/example/digid/locations.java
+++ app/src/main/java/com/example/digid/locations.java
@@ -3,8 +3,20 @@ package com.example.digid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class locations extends AppCompatActivity {
+
+    private Button refresh;
+    FirebaseAuth fAuth;
+    private ImageView dh1;
+    private ImageView dh2;
+    private TextView c1;
+    private TextView c2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
