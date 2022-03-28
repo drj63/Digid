fc78df2307f84d1e97ff4a8d4ce650bc266ebea7
Aswathy Aji <aswathyaji13@gmail.com>
2022-03-28 16:18:24 -0400

    new classes for each location

---
+++ app/src/main/java/com/example/digid/livi.java
@@ -0,0 +1,4 @@
+package com.example.digid;
+
+public class livi {
+}
--- app/src/main/java/com/example/digid/locations.java
+++ app/src/main/java/com/example/digid/locations.java
@@ -2,7 +2,10 @@ package com.example.digid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -13,14 +16,42 @@ public class locations extends AppCompatActivity {
 
     private Button refresh;
     FirebaseAuth fAuth;
-    private ImageView dh1;
-    private ImageView dh2;
-    private TextView c1;
-    private TextView c2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_locations);
+    }
+
+    public void onButtonClick(View v) {
+        if(v.getId()== R.id.c1){
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent i = new Intent(locations.this, collegeAve.class);
+                    startActivity(i);
+                    finish();
+                }
+            }, 2000);
+        }
+        if(v.getId()== R.id.c2)
+        {
+            Intent i=new Intent(locations.this, busch.class );
+            startActivity(i);
+
+        }
+        if(v.getId()== R.id.c3)
+        {
+            Intent i=new Intent(locations.this, livi.class );
+            startActivity(i);
+
+        }
+        if(v.getId()== R.id.c4)
+        {
+            Intent i=new Intent(locations.this, douglass.class );
+            startActivity(i);
+
+        }
+
     }
 }
