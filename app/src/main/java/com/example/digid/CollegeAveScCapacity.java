26585be16a87207c9a68fc0100f60b4c72e8c646
Aswathy Aji <aswathyaji13@gmail.com>
2022-03-29 15:23:29 -0400

    college ave student center arbitrary capacity value

---
+++ app/src/main/java/com/example/digid/CollegeAveScCapacity.java
@@ -0,0 +1,14 @@
+package com.example.digid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CollegeAveScCapacity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_college_ave_sc_capacity);
+    }
+}
--- app/src/main/java/com/example/digid/collegeAveCampus.java
+++ app/src/main/java/com/example/digid/collegeAveCampus.java
@@ -2,13 +2,33 @@ package com.example.digid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class collegeAveCampus extends AppCompatActivity {
+    private Button capSC;
+    private Button capAB;
+    private Button capBrow;
+    private Button capGym;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_college_ave_campus);
+
+        capSC = (Button) findViewById(R.id.sc);
+        capSC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openStudentCenter();
+            }
+        });
+    }
+    public void openStudentCenter(){
+        Intent intent = new Intent(this, CollegeAveScCapacity.class);
+        startActivity(intent);
+
     }
 }
